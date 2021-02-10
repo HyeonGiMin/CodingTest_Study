d91eb7788528062f3325bf3f7825abaad1f5413a
HyeonGiMin <hyeoni1995@gmail.com>
2021-02-10 20:50:55 +0900

    20210210

---
+++ Success/1152.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Min on 2021-02-10.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string str;
+    getline(cin,str);
+    int count = 0;
+    bool space = false;
+    if(str.empty())
+    {
+        cout << "0";
+        return 0;
+    }
+    count = 1;
+    for(int i = 0; i < str.length(); i++)
+    {
+        if(str[i] == ' ')
+            count++;
+    }
+    if(str[0] == ' ')
+        count--;
+    if(str[str.length()-1] == ' ')
+        count--;
+
+    cout << count;
+    return 0;
+}
