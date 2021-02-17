67d62861fcc5d25fd6fce57d2caec33013956e74
HyeonGiMin <hyeoni1995@gmail.com>
2021-02-17 20:29:50 +0900

    20210217

---
+++ Success/1193.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Min on 2021-02-17.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+    int idx;
+    cin >> idx;
+    int i = 1;
+    int diff = 0;
+    for(int sum = 0; sum+i < idx; i++)
+    {
+        sum += i;
+        diff = idx - sum;
+    }
+    if(i%2 == 1)
+        cout << i-diff+1 << "/" << diff;
+    else
+        cout << diff << "/" << i-diff+1;
+    return 0;
+}
