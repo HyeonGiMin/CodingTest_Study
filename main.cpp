0e2ee4aee2648000208a0c338910354b30d7ebb7
HyeonGiMin <hyeoni1995@gmail.com>
2021-02-18 22:26:42 +0900

    main reset

--- main.cpp
+++ main.cpp
@@ -3,12 +3,5 @@
 using namespace std;
 
 int main() {
-    int number;
-    cin >> number;
-    int i = 0;
-    for(int sum=2; sum <= number; i++)
-        sum += 6*i;
-    if(number == 1) i=1;
-    cout << i;
-    return 0;
+
 }
